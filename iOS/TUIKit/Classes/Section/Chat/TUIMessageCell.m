faa2e788ef0fa40440f4c900dc51cc8112da81aa
zhoujialei <dylzjl@gmail.com>
2021-03-09 14:09:13 +0800

    * 隐藏未读已读label

--- iOS/TUIKit/Classes/Section/Chat/TUIMessageCell.m
+++ iOS/TUIKit/Classes/Section/Chat/TUIMessageCell.m
@@ -63,12 +63,12 @@
         [self.contentView addSubview:_retryView];
         
         //已读label,由于 indicator 和 error，所以默认隐藏，消息发送成功后进行显示
-        _readReceiptLabel = [[UILabel alloc] init];
-        _readReceiptLabel.hidden = YES;
-        _readReceiptLabel.font = [UIFont systemFontOfSize:12];
-        _readReceiptLabel.textColor = [UIColor d_systemGrayColor];
-        _readReceiptLabel.lineBreakMode = NSLineBreakByCharWrapping;
-        [self.contentView addSubview:_readReceiptLabel];
+//        _readReceiptLabel = [[UILabel alloc] init];
+//        _readReceiptLabel.hidden = YES;
+//        _readReceiptLabel.font = [UIFont systemFontOfSize:12];
+//        _readReceiptLabel.textColor = [UIColor d_systemGrayColor];
+//        _readReceiptLabel.lineBreakMode = NSLineBreakByCharWrapping;
+//        [self.contentView addSubview:_readReceiptLabel];
         
         self.selectionStyle = UITableViewCellSelectionStyleNone;
     }
